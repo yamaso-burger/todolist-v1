a1ea30632d6a81cbe350826a1466bd8a706fec6c
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-09-04 12:54:48 +0900

    Install EJS and create list.ejs

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ var express = require('express');
 const app = express();
 var port = 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res)=>{
 
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
     if (currentDay === 6 || currentDay === 0) {
-        res.sendFile(__dirname + "/weekend.html");
+        day = "Weekend";
+
     } else {
-        res.sendFile(__dirname + "/weekday.html");
+        day = "Weekday";
+
     }
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(port, ()=>{
