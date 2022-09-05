68cbaf3b19bfcbf55b92ddd5917621afd340029e
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-09-05 11:48:12 +0900

    Make it work as ToDoList, and change the all var to let

--- app.js
+++ app.js
@@ -1,43 +1,37 @@
-var bodyParser = require ('body-parser');
-var express = require('express');
+let bodyParser = require ('body-parser');
+let express = require('express');
 
 const app = express();
-var port = 3000;
+let port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+let items = ['Add what you going to do!'];
+let isFirstAdd = true;
 
 app.get('/', (req, res)=>{
 
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    switch (currentDay){
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            day = "X Day";
-            break;
+    let today = new Date();
+    let currentDay = today.getDay();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", {kindOfDay: day, newItems: items});
+});
+
+app.post('/', (req, res)=>{
+    if(isFirstAdd) {
+        isFirstAdd = false;
+        items = [];
     }
-    
+    items.push(req.body.newItem);
+
+    res.redirect("/");
 });
 
 app.listen(port, ()=>{
